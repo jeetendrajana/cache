5739d135e0397f4c0161c9fc44856404e510d7b0
jeetendrajana <jeetendrajana@gmail.com>
2019-08-15 19:31:21 +0530

    cleanup

--- jj-async-cache/src/main/java/com/jj/test/IProductService.java
+++
@@ -1,8 +0,0 @@
-package com.jj.test;
-
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-
-public interface IProductService {
-    List<Product> getAllProducts() throws ExecutionException, InterruptedException;
-}
